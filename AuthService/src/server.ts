4b6d5407f346f1ed3c0db50d9b7714c65ff5a3a7
akansha <akansha69@.com>
2025-11-26 15:32:35 +0530

    testing

--- AuthService/src/server.ts
+++ AuthService/src/server.ts
@@ -4,18 +4,22 @@ import { serverConfig } from './config/server.config';
 import v1Router from './routers';
 import { JWTMiddleware } from './middlewares/jwt.middleware';
 import { HTTPAxiosRequest } from './apiGateway/axios';
-import { sequelize } from './models/sequelize';
 
 const app =  express();
 
 app.use(express.json());
+
+app.get('/', (req , res)=>{
+    res.send('Payment Service is up and running');
+})
 
 app.use('/api/v1' , v1Router)
 
 
 app.use(JWTMiddleware, HTTPAxiosRequest)
 
-sequelize.sync({ alter: true });
+// sequelize.sync({ alter: true });
+
 
 app.listen(serverConfig.PORT, '0.0.0.0', ()=>{
     console.log(`Listening on port : ${serverConfig.PORT}`);
